f03c8a8aa6a3abb49aaf6fcaacad1eee7c48165f
David Goffredo <dmgoffredo@gmail.com>
2018-07-31 17:40:09 -0400

    remove grossly inefficient algorithm

--- prune-types.rkt
+++ prune-types.rkt
@@ -8,18 +8,19 @@
 (define (whitespace? datum)
   (and (string? datum) (regexp-match #px"\\s+" datum)))
 
-(define (remove-with-leading-whitespace what lst)
-  (match lst
-    ; Has what we want to remove, and leading whitespace.
-    [(list before ... (? whitespace? _) (== what) after ...)
-     (remove-with-leading-whitespace what (append before after))]
+(define (remove-with-leading-whitespace remove? lst)
+  (let recur ([lst lst])
+    (match lst
+      [(list (? remove? _) tail ...)                   
+       (recur tail)]
 
-    ; Has what we want to remove, without leading whitespace.
-    [(list before ... (== what) after ...)
-     (remove-with-leading-whitespace what (append before after))]
+      [(list (? whitespace? _) (? remove? _) tail ...)
+       (recur tail)]
 
-    ; Doesn't have what we want to remove.
-    [otherwise lst]))
+      [(list head tail ...)                           
+       (cons head (recur tail))]
+
+      ['() '()])))
 
 #| Keep in mind the xexpr grammar:
 
@@ -37,20 +38,17 @@ xexpr = string
   ; missing based on the return value of the specified predicate invoked with
   ; the descendant. When a child is elided, any leading whitespace before the
   ; removed element is also removed.
-  (let ([placeholder (gensym)]) ; ID to mark children for removal
+  (let recur ([xexpr xexpr])
     (match xexpr
       [(list tag-name (list attributes ...) children ...)
        `(,tag-name 
          ,attributes 
          ; Return the children (recursing on each), but if (keep? child) is
          ; #f for any of them, remove it and any whitespace that precedes it.
-         ; A removal happens by first replacing (mapping) the child with the
-         ; removal placeholder, and then later removing the placeholder with
-         ; any preceding whitespace.
          ,@(~>> children
-             (map (lambda (child) (filter-children keep? child)))
-             (map (lambda (child) (if (keep? child) child placeholder)))
-             (remove-with-leading-whitespace placeholder)))]
+             (map (lambda (child) (recur child)))
+             (remove-with-leading-whitespace
+               (lambda (child) (not (keep? child))))))]
   
       [otherwise otherwise])))
 
